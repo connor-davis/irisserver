61b5efbde608b8499dcd77a7b058ea82d1835f87
Connor Davis <19422@kloofhigh.co.za>
2021-02-07 15:30:56 +0200

    autocommit-1612704656520

--- index.js
+++ index.js
@@ -8,11 +8,19 @@ let cors = require("cors");
 let morgan = require("morgan");
 let bodyParser = require("body-parser");
 let { ExpressPeerServer } = require("peer");
+let uuid = require("uuid");
+
+let peer = ExpressPeerServer(http, {
+    debug: false,
+    path: "/",
+    generateClientId: uuid.v4(),
+});
 
 app.use(cors());
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/p2p", peer);
 
 io.on("connection", (socket) => {
     socket.on("disconnect", () => {
