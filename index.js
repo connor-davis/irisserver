2471ab742e8e1b9625f67520e0e565e1276b0bf8
Connor Davis <19422@kloofhigh.co.za>
2021-02-07 15:24:55 +0200

    autocommit-1612704295874

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/p2p", peer);
 
 io.on("connection", (socket) => {
-    socket.on("_peerInitialized", ({ id }) => {
-        console.log(id);
+    socket.on("_peerInitialized", ({ id, peerId }) => {
+        console.log(id, peerId);
     });
 
     socket.on("disconnect", () => {
