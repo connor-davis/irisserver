2e46aeb17ac86ab48c3125b277b114fb797bc0c2
Connor Davis <19422@kloofhigh.co.za>
2021-02-07 14:58:24 +0200

    autocommit-1612702704071

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ let http = require("http").createServer(app);
 let io = require("socket.io")(http);
 let cors = require("cors");
 let morgan = require("morgan");
+let bodyParser = require("body-parser");
 
 app.use(cors());
-app.use(morgan('combined'))
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 io.on("connection", (socket) => {
     console.log("new connection.");
